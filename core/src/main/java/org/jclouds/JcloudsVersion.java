812a81e8ea3a98ae68fea0f17e9e5e9e77ba4805
Andrea Turli <andrea.turli@gmail.com>
2016-07-08 11:13:11 +0200

    change VERSION_RESOURCE_FILE and SEMANTIC_VERSION_PATTERN in JcloudsVersion

--- core/src/main/java/org/jclouds/JcloudsVersion.java
+++ core/src/main/java/org/jclouds/JcloudsVersion.java
@@ -33,15 +33,15 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class JcloudsVersion {
     @VisibleForTesting
-    static final String VERSION_RESOURCE_FILE = "META-INF/maven/org.apache.jclouds/jclouds-core/pom.properties";
+    static final String VERSION_RESOURCE_FILE = "META-INF/maven/io.cloudsoft.jclouds/jclouds-core/pom.properties";
     private static final String VERSION_PROPERTY_NAME = "version";
 
     /*
      * x.y.z or x.y.z-alpha.n or x.y.z-beta.n or x.y.z-rc.n or x.y.z-SNAPSHOT -
      * see http://semver.org.
      */
-    private static final Pattern SEMANTIC_VERSION_PATTERN =
-        Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)(?:-(alpha|beta|rc)\\.(\\d+)|-SNAPSHOT)?");
+    private static final Pattern SEMANTIC_VERSION_PATTERN = Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)(?:-(alpha|beta|rc|alpha-cloudsoft|ea-cloudsoft|cloudsoft)\\.(\\d+)|-SNAPSHOT)?");
+
     private static final String ALPHA_VERSION_IDENTIFIER = "alpha";
     private static final String BETA_VERSION_IDENTIFIER = "beta";
 
