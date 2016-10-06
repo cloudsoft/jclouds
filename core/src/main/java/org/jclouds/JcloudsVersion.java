86099d6d68d10bfbab24cf9e2c7abf8a4d613da0
Guglielmo Nigri <guglielmo.nigri@cloudsoftcorp.com>
2016-10-06 12:25:48 +0200

    Custom Cloudsoft jclouds 1.9.3-beta.20161006 release

--- core/src/main/java/org/jclouds/JcloudsVersion.java
+++ core/src/main/java/org/jclouds/JcloudsVersion.java
@@ -33,7 +33,7 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class JcloudsVersion {
     @VisibleForTesting
-    static final String VERSION_RESOURCE_FILE = "META-INF/maven/org.apache.jclouds/jclouds-core/pom.properties";
+    static final String VERSION_RESOURCE_FILE = "META-INF/maven/io.cloudsoft.jclouds/jclouds-core/pom.properties";
     private static final String VERSION_PROPERTY_NAME = "version";
 
     /*
